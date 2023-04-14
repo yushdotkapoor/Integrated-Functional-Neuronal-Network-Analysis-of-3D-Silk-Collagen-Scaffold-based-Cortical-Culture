417dac19cdb80b26d74ea28f86c5d9e4600de1e7
Yush Kapoor <YushDotKapoor@gmail.com>
2023-04-14 14:52:47 -0400

    matrix extraction allows user input
    In the case where all Functional correlation matrices have been created, in processed_analysis.mat, the user can choose exactly which functional correlation method to use.

--- Matlab Code-Integrated Functional Neuronal Network Analysis of 3D Silk-Collagen Scaffold-based Cortical/matrixExtraction.m
+++ Matlab Code-Integrated Functional Neuronal Network Analysis of 3D Silk-Collagen Scaffold-based Cortical/matrixExtraction.m
@@ -4,7 +4,7 @@ function [A]=matrixExtraction(processed_analysis)
 switch processed_analysis.params.FC.method_idx
     case 1
         A=processed_analysis.FC.CC.C;
-        fprintf('Functional connectivity method detected:Cross-correlation\n')
+        fprintf('Functional connectivity method detected: Cross-correlation\n')
     case 2
         A=processed_analysis.FC.PC.C;
         fprintf('Functional connectivity method detected: Partial correlation\n')
@@ -17,5 +17,14 @@ switch processed_analysis.params.FC.method_idx
     case 5
         A=processed_analysis.FC.TE.C;
         fprintf('Functional connectivity method detected: Transfer Entropy\n')
+    case 6
+        fprintf('All Functional Connectivity Methods are active, but there can only be one.\n')
+        msg = "Choose Functional Connectivity Method";
+        opts = ["Cross-correlation" "Partial correlation" "Instantaneous phase" "Granger causality", "Transfer Entropy"];
+        choice = menu(msg,opts);
+        processed_analysis.params.FC.method_idx = choice;
+        A = matrixExtraction(processed_analysis);
+        
+        
 end
 end
